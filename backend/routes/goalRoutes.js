4efeab3d301a8202a04aa0abaada7f5e5d5b1f94
ahad.hamirani <ahadameen@hotmail.com>
2022-06-27 23:55:19 -0600

    Added goal router controller

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-});
+router.route("/").get(getGoals).post(setGoal);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set Goals" });
-});
+router.route("/:id").delete(deleteGoal).put(updateGoal);
 
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
 module.exports = router;
